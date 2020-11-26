d64a926c01d47024792a03a0e5bed05931e0459a
El Med Tacos <ahmed.tmtc.live.fr@gmail.com>
2020-11-26 14:31:05 +0100

    fix #All: Completed all duties

--- src/main/main/Pizza.java
+++ src/main/main/Pizza.java
@@ -3,32 +3,39 @@ package main;
 import java.util.ArrayList;
 
 abstract public class Pizza {
-	 String nom;
-	 String pate;
-	 String sauce;
-	 ArrayList<String> garnitures = new ArrayList<String>();
-	 
-	 public String getNom() {
-		 return nom;
-	 }
+	protected String nom;
+	protected String pate;
+	protected String sauce;
+	protected ArrayList<String> garnitures;
 
-	 public void preparer() {
+	protected Pizza() {
+		this.nom = "";
+		this.pate = "";
+		this.sauce = "";
+		this.garnitures = new ArrayList<String>();
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void preparer() {
 		System.out.println("Preparation de Pizza\nEtalage de la pate... \nAjout de la sauce... \nAjout des garnitures: ");
-		
+
 		for (int i=0; i<garnitures.size(); i++) {
 			System.out.println("  " + garnitures.get(i));
 		}
-	 }
+	}
 
-	 public void cuire() {
-		 System.out.println("Cuisson 25 minutes a 180°");
-	 }
+	public void cuire() {
+		System.out.println("Cuisson 25 minutes a 180°");
+	}
 
-	 public void couper() {
-		 System.out.println("Decoupage par default");
-	 }
+	public void couper() {
+		System.out.println("Decoupage par default");
+	}
 
-	 public void emballer() {
-		 System.out.println("Emballage dans une boite officielle");
-	 }
+	public void emballer() {
+		System.out.println("Emballage dans une boite officielle");
+	}
 }
